1059ac2eb1df4188e48409a2386a6ef6c9a35be4
ThanatpornMilk <milktnp46@gmail.com>
2024-11-24 23:27:01 +0700

    สร้างหน้า page1

--- src/app/utils/page1.tsx
+++ src/app/utils/page1.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Page1 = () => {
   return (
-    <div className='mx-24 my-16'>
-      <header>hello</header>
+    <div className='mx-24 my-16 min-h-screen'>
+      <div className='flex items-center text-[#FFF9EF] text-8xl font-bold'>
+        <span>Share your project</span>
+        <img src="/img2.png" className="w-24 h-24 ml-6 relative -translate-y-6" alt="dec2" 
+        />
+      </div>
+      <div className='my-6 text-[#FDE69E] text-8xl font-bold flex items-center'>
+        <span className="mr-6">with other</span>
+        <img src="/img1.png" className="w-20 h-20" alt="dec1" />
+      </div>      <div className='mt-16 text-[#FFF9EF] text-2xl'>ร่วมแชร์ผลงานในรายวิชาร่วมกับเพื่อน ๆ</div>
+      <div className='my-3 text-[#4A4D4D] text-2xl flex justify-between items-center'>หรือเข้าชมผลงานของรุ่นพี่ในแต่ละรายวิชา ก่อนทำการตัดสินใจลงทะเบียน
+        <a href="#page2">
+          <button type="button" className='bg-[#FDE69E] text-[#4A4D4D] text-xl font-medium py-2 px-6 rounded-[15px] hover:bg-[#FFF9EF] transition-colors duration-300 mr-10'>Get Start</button>
+        </a>
+      </div>
     </div>
   )
 }
