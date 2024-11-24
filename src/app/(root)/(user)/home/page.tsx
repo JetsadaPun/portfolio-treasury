aec6ab20bde41791d12dc7ab62d4a547461ccc35
JetsadaPun <142483437+JetsadaPun@users.noreply.github.com>
2024-11-24 16:39:40 +0700

    ีcreate new page

---
+++ src/app/(root)/(user)/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Page1 from '../../../utils/page1';
+import Page2 from '../../../utils/page2';
+const page = () => {
+  return (
+    <div>pageabout
+      <Page1/>
+      <Page2/>
+    </div>
+  )
+}
+
+export default page
